523908dc0107bd564284beaf8c8d646e1ef71beb
fatmaCevik <cevikfatma93@gmail.com>
2023-11-25 15:04:38 +0300

    Fix in Cube Generator

--- Cube-Surfer/Assets/Scripts/PlayScript/CubeGenerator.cs
+++ Cube-Surfer/Assets/Scripts/PlayScript/CubeGenerator.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class CubeGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject cubePrefab;
+    [SerializeField] private GameObject cubes;
     [SerializeField] private float cubeSpacing = 1f;
     [SerializeField] private int cubeCount = 5;
-    [SerializeField] private float startingHeight = 1f;
 
     private List<GameObject> generatedCubes = new List<GameObject>();
 
@@ -25,11 +25,11 @@ public class CubeGenerator : MonoBehaviour
     {
         for (int i = 0; i < cubeCount; i++)
         {
-            GameObject cube = Instantiate(cubePrefab, transform.position + Vector3.up * i * cubeSpacing, Quaternion.identity);
+            GameObject cube = Instantiate(cubePrefab, transform.localPosition + Vector3.up * i * cubeSpacing, Quaternion.identity);
 
             generatedCubes.Add(cube);
 
-            cube.transform.SetParent(transform, true); //Sefa'ya sor!!
+            cube.transform.SetParent(cubes.transform, false); //Sefa'ya sor!!
         }
     }
 
