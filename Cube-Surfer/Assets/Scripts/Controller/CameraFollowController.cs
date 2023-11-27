8369c73b36f62deb9a6824bb3889ca132f129c85
fatmaCevik <cevikfatma93@gmail.com>
2023-11-27 14:41:46 +0300

    Correction
    SwerveParent was placed instead of Player in the PlayerModel field on SwerveController. Movement on the x-axis was provided in the cubes with Stickman.

--- Cube-Surfer/Assets/Scripts/Controller/CameraFollowController.cs
+++ Cube-Surfer/Assets/Scripts/Controller/CameraFollowController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class CameraFollowController : MonoBehaviour
 {/*
     [SerializeField] private Transform player;
     [SerializeField] private Vector3 offset;
