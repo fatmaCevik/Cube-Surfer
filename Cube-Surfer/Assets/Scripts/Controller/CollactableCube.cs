077d326a726c28ff4a3ac0592df36ace93fed2fb
fatmaCevik <cevikfatma93@gmail.com>
2023-12-06 20:50:22 +0300

    Edit

--- Cube-Surfer/Assets/Scripts/Controller/CollactableCube.cs
+++ Cube-Surfer/Assets/Scripts/Controller/CollactableCube.cs
@@ -1,9 +1,20 @@
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class CollactableCube : MonoBehaviour
 {
     [SerializeField] private LayerMask obstacleLayer;
+
     public bool isCollected;
+
+    private Collider cubeCollider;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        cubeCollider = GetComponent<Collider>();
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,6 +23,9 @@ public class CollactableCube : MonoBehaviour
         {
             CubeGenerator.Instance.LeaveCubesOnObstacle();
             otherObject.GetComponent<Collider>().enabled = false;
+            cubeCollider.enabled = false;
+            Destroy(rb);
+            
         }
     }
 }
